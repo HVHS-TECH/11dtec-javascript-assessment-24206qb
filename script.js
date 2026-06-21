7fb8bf16fb69b8b1b77412d9fc1ea383bde9ad39
Quoc Bao <24206qb@hvhs.school.nz>
2026-06-21 21:55:21 +0000

    making add item to cart

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 console.log("Hello world!")
 
-const ShopItems = ["Axe", "Sword", "mace"]
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
+function addShoppingItem() {
+  const shoppingInput = document.getElementById("shoppingItemField");
+  const item = shoppingInput.value.trim();
+  SHOPPING_LIST.forEach((item) => {
+    message += `<p>${item}</p>`;
+  });
 
+  SHOPPING_OUTPUT.innerHTML = message;
+}
