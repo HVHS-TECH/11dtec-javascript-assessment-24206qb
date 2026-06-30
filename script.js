0cebd3ee86f6af513f8476432a28b1e911451fb2
Quoc Bao <24206qb@hvhs.school.nz>
2026-06-30 20:24:32 +0000

    Add note, and can now add price and name of a product

--- script.js
+++ script.js
@@ -1,68 +1,36 @@
 console.log('hello world');
 const OUTPUT = document.getElementById("listcart");
+const listitem = []; //array//
 
 function addToCart(name, price) {
-OUTPUT.innerHTML += "<p>"+ name + "" + price + "</p>";
+
+    listitem.push({name: name, price: price}); //add the price and name producet to the array//
+    showCart();
 }
 
+function showCart(){
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function openCart() {
-const cartTab = document.querySelector(".showcart .cartTab")
-
- if (cartTab) {
-   cartTab.style.inset =" 0 0px 0 auto ";
+   OUTPUT.innerHTML = ""; //It remove item from the array//
+   for (let i = 0; i < listitem.length; i++) {//i start at 0 until equan to the number of product, and i plus one every time//
+   OUTPUT.innerHTML += "<p>" + listitem[i].name + "-" + listitem[i].price + "</p>";
  } 
 }
 
-function closeCart() {
+
+
+function openCart() { // open //
+const cartTab = document.querySelector(".showcart .cartTab") //find the item you want to change//
+
+ if (cartTab) { //if statement//
+   cartTab.style.inset =" 0 0px 0 auto "; //change the from -600px to 0px//
+ } 
+}
+
+function closeCart() { // close //
 const cartTab = document.querySelector(".showcart .cartTab")
 
  if (cartTab) {
-   cartTab.style.inset =" 0 -800px 0 auto ";
+   cartTab.style.inset =" 0 -600px 0 auto "; // revese the change//
  }
   
 }
